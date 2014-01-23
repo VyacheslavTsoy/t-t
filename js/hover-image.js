44d147f66b8ec5f1f394741fc4db96e4ccb6bf82
wingart <wingart.team@gmail.com>
2014-01-23 23:56:18 +0200

    last update

--- js/hover-image.js
+++
@@ -1,1 +0,0 @@
-$(function(){$(".gallery").append("<span></span>");$(".gallery").hover(function(){$(this).find("img").stop().animate({opacity:0.5},"normal")},function(){$(this).find("img").stop().animate({opacity:1},"normal")})});
